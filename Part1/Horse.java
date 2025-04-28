189aa518bfd601171b4322529edbfb97cdcfdde7
DarkinIV <muhammedasim89@gmail.com>
2025-04-28 15:14:58 +0100

    Encapsulation
    Encapsulation for the setter method 'setConfidence'.

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -67,10 +67,16 @@ public class Horse
         this.distanceTravelled += 1;
     }
 
-    public void setConfidence(double newConfidence)
-    {
-        this.horseConfidence = newConfidence;
+    public void setConfidence(double newConfidence) {
+        if (newConfidence < 0) {
+            this.horseConfidence = 0;
+        } else if (newConfidence > 1) {
+            this.horseConfidence = 1;
+        } else {
+            this.horseConfidence = newConfidence;
+        }
     }
+    
     
     public void setSymbol(char newSymbol)
     {
