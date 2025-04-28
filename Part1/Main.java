154d9978937bc1b87b3c7673587893359e19f617
DarkinIV <muhammedasim89@gmail.com>
2025-04-28 12:40:40 +0100

    Horse creation
    Generated 3 horses for the race

--- Part1/Main.java
+++ Part1/Main.java
@@ -3,12 +3,12 @@ class Main{
     public static void main(String args[]){
         Race race = new Race(25);
 
-        Horse apple = new Horse('♞', "Apple", 0.4);
-        Horse bumblebee = new Horse('♘', "Bumblebee", 0.5);
-        Horse chevy = new Horse('♞', "Chevy", 0.6);
+        Horse apples = new Horse('♞', "Apples", 0.4);
+        Horse butter = new Horse('♘', "Butter", 0.5);
+        Horse chevy = new Horse('♕', "Chevy", 0.6);
 
-        race.addHorse(apple, 1);
-        race.addHorse(bumblebee, 2);
+        race.addHorse(apples, 1);
+        race.addHorse(butter, 2);
         race.addHorse(chevy, 3);
 
         race.startRace();
